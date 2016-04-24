0490a777c2fe7c6074d5ac159267244d5333db0a
David Boclé <bcldvd@gmail.com>
2016-04-24 09:27:51 +0000

    apply data + handle API changes

--- app.js
+++ app.js
@@ -5,10 +5,10 @@
 			.controller('MainController', MainController);
 
 	function MainController($scope) {
-	    
-      blockspring.runParsed("4f791c674f8ef3c0b80d28d2042eb6d4", { }, function(res){
+      blockspring.runParsed("4f791c674f8ef3c0b80d28d2042eb6d4", {cache: false }, function(res){
         console.log(res.params);
         $scope.adnennary = res.params.adnennary;
+        $scope.$apply();
       });
 	}
 	
