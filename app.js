bb3089ac5c01a31497b19635150600e5d4372b3d
David Boclé <bcldvd@gmail.com>
2016-04-24 09:17:19 +0000

    First commit from c9

---
+++ app.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+	angular
+			.module('app', [])
+			.controller('MainController', MainController);
+
+	function MainController($scope) {
+	    
+      blockspring.runParsed("4f791c674f8ef3c0b80d28d2042eb6d4", { }, function(res){
+        console.log(res.params);
+        $scope.adnennary = res.params.adnennary;
+      });
+	}
+	
+	
+})();
